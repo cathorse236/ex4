d98bc8bd1855cc6970f51486320184f918b69aff
cathorse236 <s1280236@u-aizu.ac.jp>
2021-07-02 16:30:07 +0900

    Won/lost message added

--- ex4.c
+++ ex4.c
@@ -12,6 +12,8 @@ int main() {
 	b = rand() % 6 + 1;
 	printf("Die 2: %d\n", b);
 	printf("Total value: %d\n", a+b);
+	if (a + b > 7) printf("You won.\n");
+	else printf("You lost.\n");
 
 	return 0;
 }
